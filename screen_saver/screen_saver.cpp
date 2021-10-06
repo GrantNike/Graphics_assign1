f3e73d808d1453dd68f88f19ebd9736ede9d2d78
GrantNike <grant.nike001@protonmail.com>
2021-10-06 19:18:03 -0400

    fixed

--- screen_saver/screen_saver.cpp
+++ screen_saver/screen_saver.cpp
@@ -18,7 +18,7 @@ Screen Saver Application
 #include <chrono>
 #include <thread>
 
-enum{MENU_SLOW, MENU_FAST, MENU_RESET, MENU_QUIT};
+enum{MENU_SLOW, MENU_FAST, MENU_ROTATE, MENU_RESET, MENU_QUIT};
 
 typedef struct {
 	GLfloat x,y;
@@ -32,19 +32,23 @@ typedef struct {
 typedef struct {
 	point p1;
     point p2;
-    GLuint square_width = 100;
-    GLuint square_height = 100;
+    GLfloat screen_width = 1920.0;
+    GLfloat screen_height = 1080.0;
+    GLuint square_width = 300;
+    GLuint square_height = 200;
     velocity v;
-    int busy_sleep = 1;
+    float busy_sleep = 10;
+    int twoTimes = 10;
+    int range = 5;
 } glob;
 glob global;
 
 void square() {
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POLYGON);
-        glColor3f(1.0, 0.0, 0.0);
+        glColor3f(0, 1.0, 0);
 	glVertex2i(global.p1.x, global.p1.y);
-        glColor3f(0.0, 1.0, 0.0);
+        glColor3f(1.0, 0.0, 0.0);
 	glVertex2i(global.p1.x, global.p2.y);
         glColor3f(0.0, 0.0, 1.0);
 	glVertex2i(global.p2.x, global.p2.y);
@@ -52,52 +56,51 @@ void square() {
 	glVertex2i(global.p2.x, global.p1.y);
     glEnd();
     glutSwapBuffers();
-    glFlush();
 }
 
 void start_square(){
-    global.p1.x = random()%(799-global.square_width);
-    global.p1.y = random()%(799-global.square_height);
+    global.p1.x = random()%((int)global.screen_width-global.square_width);
+    global.p1.y = random()%((int)global.screen_height-global.square_height);
     global.p2.x = global.p1.x+global.square_width;
     global.p2.y = global.p1.y+global.square_height;
     int rand;
     do{
-        rand = (random()%(10))-5;
-    }while(rand == 0);
-    global.v.x = rand*0.1;
+        rand = (random()%(global.twoTimes))-global.range;
+    }while(rand < 2 && rand > -2);
+    global.v.x = rand;
     do{
-        rand = (random()%(10))-5;
-    }while(rand == 0);
-    global.v.y = rand*0.1;
+        rand = (random()%(global.twoTimes))-global.range;
+    }while(rand < 2 && rand > -2);
+    global.v.y = rand;
     square();
 }
 
 void animate(int value){
-    glutTimerFunc(global.busy_sleep, animate, 0);
     int offset = 0;
-    if(global.p1.x + global.v.x >= offset && global.p2.x + global.v.x <= 799){
+    if(global.p1.x + global.v.x >= offset && global.p2.x + global.v.x <= global.screen_width){
         global.p1.x += global.v.x;
         global.p2.x += global.v.x;
     }
     else{
         int rand;
         do{
-            rand = (random()%(10))-5;
-        }while(rand == 0);
-        global.v.x = rand*0.1;
+            rand = (random()%(global.twoTimes))-global.range;
+        }while(rand < 2 && rand > -2);
+        global.v.x = rand;
     }
-    if(global.p1.y + global.v.y >= offset && global.p2.y + global.v.y <= 799){
+    if(global.p1.y + global.v.y >= offset && global.p2.y + global.v.y <= global.screen_height){
         global.p1.y += global.v.y;
         global.p2.y += global.v.y;
     }
     else{
         int rand;
         do{
-            rand = (random()%(10))-5;
-        }while(rand == 0);
-        global.v.y = rand*0.1;
+            rand = (random()%(global.twoTimes))-global.range;
+        }while(rand < 2 && rand > -2);
+        global.v.y = rand;
     }
     glutPostRedisplay();
+    glutTimerFunc(global.busy_sleep, animate, 0);
     //std::this_thread::sleep_for(std::chrono::milliseconds(global.busy_sleep));
 }
 
@@ -109,6 +112,14 @@ void menu_func(int value){
             break;
         case MENU_FAST:
             global.busy_sleep = global.busy_sleep > 1 ? global.busy_sleep/2 : 1;
+            break;
+        case MENU_ROTATE:
+            {
+                int temp = global.square_width;
+                global.square_width = global.square_height;
+                global.square_height = temp;
+                start_square();
+            }
             break;
         case MENU_QUIT:
             exit(0);
@@ -126,6 +137,7 @@ void create_menu(){
     glutAddMenuEntry("Reset", MENU_RESET);
     glutAddMenuEntry("Slow Time", MENU_SLOW);
     glutAddMenuEntry("Speed Up Time", MENU_FAST);
+    glutAddMenuEntry("Rotate Shape", MENU_ROTATE);
     glutAddMenuEntry("Quit", MENU_QUIT);
     glutAttachMenu(GLUT_RIGHT_BUTTON);
 }
@@ -155,26 +167,30 @@ void keyboard(unsigned char key, int x, int y){
 }
 
 void show_keys(){
-	std::cout<<(random()%2)-1;
+	printf("r = Reset\n");
+    printf("s = Slow Time\n");
+    printf("f = Speed Up Time\n");
+    printf("q = Quit\n");
 }
 
 main(int argc, char **argv){
+    show_keys();
     //Create random seed for random number generation
 	srand(time(NULL));
     glutInit(&argc, argv);
+    glutInitWindowSize(1920/2,1080/2);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutCreateWindow("Screen Saver");
-    glutFullScreen();
+    //glutFullScreen();
 
     start_square();
     glutDisplayFunc(square);
     //glutIdleFunc(animate);
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(0.0, 799.0, 0.0, 799.0); 
+    gluOrtho2D(0.0, global.screen_width, 0.0, global.screen_height); 
     glutKeyboardFunc(keyboard);
     create_menu();
-    show_keys();
     glutTimerFunc(global.busy_sleep, animate, 0);
 
     glutMainLoop();
